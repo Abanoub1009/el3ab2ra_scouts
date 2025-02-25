4740d2d560f09130dc83dfbb840c958285e5aad1
Abanoub1009 <bibosoliman20@icloud.com>
2025-02-25 23:29:43 +0200

    Initial commit

---
+++ Scripts/fetchData.js
@@ -0,0 +1,12 @@
+﻿async function fetchData() {
+    try {
+        const response = await fetch('https://localhost:44381/Db/match.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
